3aca48dadd9b6ff92583de613c300101e3c4e3fa
AnnaAbduvaleeva <90334337+AnnaAbduvaleeva@users.noreply.github.com>
2022-02-08 21:28:12 +0300

    Add files via upload

---
+++ 01_laboratornaya/08_lab.py
@@ -0,0 +1,8 @@
+print('Введите имя пользователя:')
+first_name = input()
+greeting = 'Привет,'
+print(greeting, first_name)
+intro = 'К сожалению, у Вас нет доступа к системе.'
+info = 'Пожалуйста, обратитесь к системному администратору.'
+print(intro)
+print(info)
---
+++ 01_laboratornaya/09_lab.py
@@ -0,0 +1,5 @@
+print('Введите город вылета:')
+departure_city = input()
+print('Введите город прилета:')
+arrival_city = input()
+print(departure_city, '-', arrival_city)
