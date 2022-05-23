cff282324b567323771abcb28b692703774fcff7
AnnaAbduvaleeva <90334337+AnnaAbduvaleeva@users.noreply.github.com>
2022-05-23 09:52:30 +0300

    Delete 01_laboratornaya directory

--- 01_laboratornaya/08_lab.py
+++
@@ -1,8 +0,0 @@
-print('Введите имя пользователя:')
-first_name = input()
-greeting = 'Привет,'
-print(greeting, first_name)
-intro = 'К сожалению, у Вас нет доступа к системе.'
-info = 'Пожалуйста, обратитесь к системному администратору.'
-print(intro)
-print(info)
--- 01_laboratornaya/09_lab.py
+++
@@ -1,5 +0,0 @@
-print('Введите город вылета:')
-departure_city = input()
-print('Введите город прилета:')
-arrival_city = input()
-print(departure_city, '-', arrival_city)
